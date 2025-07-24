0c92cf42a3f2985173acb000dc67f20586631412
Kathryn Atherton <40177040+k-atherton@users.noreply.github.com>
2025-07-24 11:37:16 +0100

    Update 06_run_calculate_diversity_and_function.sh

--- 06_Calculate_Diversity_and_Function/06_run_calculate_diversity_and_function.sh
+++ 06_Calculate_Diversity_and_Function/06_run_calculate_diversity_and_function.sh
@@ -1,19 +1,13 @@
 #!/bin/bash -l
 
-# Set SCC project
-#$ -P microbiome
-
 # Send an email when the job finishes or if it is aborted (by default no email is sent).
 #$ -m beas
-
-# Give job a name
-#$ -N memento_diversity
 
 # Combine output and error files into a single file
 #$ -j y
 
 # Specify the output file name
-#$ -o Bash_Outputs/MEMENTO_atherton_calculate_diversity_function_bash_output.txt
+#$ -o calculate_diversity_function_bash_output.txt
 
 # Keep track of information related to the current job
 echo "=========================================================="
@@ -21,31 +15,31 @@ echo "Start date : $(date)"
 echo "Job name : $JOB_NAME"
 echo "=========================================================="
 
-script_dir="/projectnb/talbot-lab-data/Katies_data/Amplicon_Sequence_Cleaning"
-project_dir="/projectnb/talbot-lab-data/Katies_data/MEMENTO"
+script_dir="<path to Amplicon_Sequence_Cleaning directory>"
+project_dir="<path to project directory>"
 
 echo "Running on 16S data"
 module load R/4.3.1 
 Rscript $script_dir/06_Calculate_Diversity_and_Function/06_calculate_diversity_and_function.R \
-	-a "16S" \
-	-n "atherton" \
-	-p $project_dir \
-	-m "$project_dir/01_Collect_Data/01_Sample_Metadata/MEMENTO_atherton_16S_sample_metadata_dada2_dropsamples_decontam_20250510.csv" \
+	-a "16S" \ # amplicon sequence type [options: 16S or ITS]
+	-n "atherton" \ # your last name, used for naming output files so we know who ran the script
+	-p $project_dir \ # path to where outputs should be saved
+	-m <path to metadata file> \ # see documentation
 	-s $script_dir \
-	-e "Y" \
-	-r "$project_dir/02_Clean_Data/05_Transform_Data/16S/Rarefied_ASV_Tables/" \
-	-t "$project_dir/02_Clean_Data/02_DADA2_ASV_Tables/16S/atherton_16S_taxonomy_allsampletypes_raw_20250507.csv" \
-	--pathogen16s "/projectnb/talbot-lab-data/Katies_data/M-BUDS/03_Data_Analysis/02_Identify_Pathogens/atherton_16S_mbpd_blast_match_table_20240925.txt"
+	-e "Y" \ # do you want to edit the metadata file to add the DADA2 read count? [options: Y or N]
+	-r <path to directory with rarefied data> \ # see documentation
+	-t <path to taxonomy table> \ # see documentation
+	--pathogen16s <path to 01_blast_16s_pathogens.sh output> # see documentation
 
 echo "Running on ITS data"
 Rscript $script_dir/06_Calculate_Diversity_and_Function/06_calculate_diversity_and_function.R \
-	-a "ITS" \
-	-n "atherton" \
-	-p $project_dir \
-	-m "$project_dir/01_Collect_Data/01_Sample_Metadata/MEMENTO_atherton_ITS_sample_metadata_dada2_dropsamples_decontam_20250510.csv" \
+	-a "ITS" \ # amplicon sequence type [options: 16S or ITS]
+	-n "atherton" \ # your last name, used for naming output files so we know who ran the script
+	-p $project_dir \ # path to where outputs should be saved
+	-m <path to metadata file> \ # see documentation
 	-s $script_dir \
-	-e "Y" \
-	-r "$project_dir/02_Clean_Data/05_Transform_Data/ITS/Rarefied_ASV_Tables/" \
-	-t "$project_dir/02_Clean_Data/02_DADA2_ASV_Tables/ITS/atherton_ITS_taxonomy_allsampletypes_raw_20250507.csv"
+	-e "Y" \ # do you want to edit the metadata file to add the DADA2 read count? [options: Y or N]
+	-r <path to directory with rarefied data> \ # see documentation
+	-t <path to taxonomy table> # see documentation
 
 echo "Done."
