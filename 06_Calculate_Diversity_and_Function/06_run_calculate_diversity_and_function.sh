098144dd5cb7dd8420a9f1889d77bb864d1dc50c
Kathryn Atherton <40177040+k-atherton@users.noreply.github.com>
2025-07-24 12:01:06 +0100

    Update 06_run_calculate_diversity_and_function.sh

--- 06_Calculate_Diversity_and_Function/06_run_calculate_diversity_and_function.sh
+++ 06_Calculate_Diversity_and_Function/06_run_calculate_diversity_and_function.sh
@@ -20,26 +20,38 @@ project_dir="<path to project directory>"
 
 echo "Running on 16S data"
 module load R/4.3.1 
+
+# Input information: 
+# -a: amplicon sequence type [options: 16S or ITS]
+# -n: your last name, used for naming output files so we know who ran the script
+# -p: path to where outputs should be saved
+# -m: path to metadata file, see documentation
+# -s: path to script directory
+# -e: do you want to edit the metadata file to add the DADA2 read count? [options: Y or N]
+# -r: path to directory with rarefied data, see documentation
+# -t: path to taxonomy table created by Step 2, see documentation
+# --pathogen16s: path to 01_blast_16s_pathogens.sh output, only needed for 16S data; see documentation
+
 Rscript $script_dir/06_Calculate_Diversity_and_Function/06_calculate_diversity_and_function.R \
-	-a "16S" \ # amplicon sequence type [options: 16S or ITS]
-	-n "atherton" \ # your last name, used for naming output files so we know who ran the script
-	-p $project_dir \ # path to where outputs should be saved
-	-m <path to metadata file> \ # see documentation
+	-a "16S" \
+	-n "atherton" \
+	-p $project_dir \
+	-m <path to metadata file> \
 	-s $script_dir \
-	-e "Y" \ # do you want to edit the metadata file to add the DADA2 read count? [options: Y or N]
-	-r <path to directory with rarefied data> \ # see documentation
-	-t <path to taxonomy table> \ # see documentation
-	--pathogen16s <path to 01_blast_16s_pathogens.sh output> # see documentation
+	-e "Y" \
+	-r <path to directory with rarefied data> \
+	-t <path to taxonomy table> \
+	--pathogen16s <path to 01_blast_16s_pathogens.sh output>
 
 echo "Running on ITS data"
 Rscript $script_dir/06_Calculate_Diversity_and_Function/06_calculate_diversity_and_function.R \
-	-a "ITS" \ # amplicon sequence type [options: 16S or ITS]
-	-n "atherton" \ # your last name, used for naming output files so we know who ran the script
-	-p $project_dir \ # path to where outputs should be saved
-	-m <path to metadata file> \ # see documentation
+	-a "ITS" \
+	-n "atherton" \
+	-p $project_dir \
+	-m <path to metadata file> \
 	-s $script_dir \
-	-e "Y" \ # do you want to edit the metadata file to add the DADA2 read count? [options: Y or N]
-	-r <path to directory with rarefied data> \ # see documentation
-	-t <path to taxonomy table> # see documentation
+	-e "Y" \
+	-r <path to directory with rarefied data> \
+	-t <path to taxonomy table> \
 
 echo "Done."
